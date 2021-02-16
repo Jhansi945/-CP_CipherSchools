6694f62b3219fbdb859064d58426b7b887d994a4
Jhansi945 <63444391+Jhansi945@users.noreply.github.com>
2021-02-16 06:11:03 +0530

    Add files via upload

---
+++ DAY02-RECURSION/LAST NON ZERO DIGIT FACT.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int dig[]={1,1,2,6,4,2,2,4,2,8};
+int last(int n)
+{
+	if(n<10)
+	{
+		return dig[n];
+	}
+	if(((n/10)%10)%2==0)
+	{
+		return (6*last(n/5)*dig[n%10])%10;
+	}
+	else
+	{
+		return (4*last(n/5)*dig[n%10])%10;
+	}
+}
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<last(n)<<endl;
+	return 0;
+}
